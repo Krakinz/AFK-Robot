782e7e64c1016140cdf8548144a35950d60a76ed
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-10-09 09:59:13 +0300

    ╰☆☆••| 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐀𝐟𝐤 𝐁𝐨𝐭 |••☆☆╮

---
+++ AWAY/__init__.py
@@ -0,0 +1,56 @@
+"""
+•=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=•
+                                                       GNU GENERAL PUBLIC LICENSE 
+                                                         Version 3, 29 June 2007
+                                                Copyright (C) 2007 Free Software Foundation
+                                            Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
+                                                of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+                                                has been licensed under GNU General Public License
+                                                𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+•=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=•
+"""
+from HYPE_AFK_BOT import LOAD, LOGGER, NO_LOAD
+from termcolor import colored, cprint
+
+
+def __list_all_modules():
+    import glob
+    from os.path import basename, dirname, isfile
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    if LOAD or NO_LOAD:
+        to_load = LOAD
+        if to_load:
+            if not all(
+                any(mod == module_name for module_name in all_modules)
+                for mod in to_load
+            ):
+                LOGGER.error("Invalid loadorder names. Quitting.")
+                quit(1)
+
+            all_modules = sorted(set(all_modules) - set(to_load))
+            to_load = list(all_modules) + to_load
+
+        else:
+            to_load = all_modules
+
+        if NO_LOAD:
+            LOGGER.info("Not loading: {}".format(NO_LOAD))
+            return [item for item in to_load if item not in NO_LOAD]
+
+        return to_load
+
+    return all_modules
+
+
+ALL_MODULES = __list_all_modules()
+cprint(f"               —🚸••÷[ Hყρҽ Aϝƙ Bσƚ ]÷••🚸—", 'yellow')
+cprint(f"                      —🖥 Dҽʋ Mҽɳƚισɳ: ", 'red')
+cprint(f"                   @Krakinz | @KrakinzBot", 'green')
+LOGGER.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
--- AWAY/afk.py
+++ AWAY/afk.py
@@ -1,1 +1,189 @@
-...
+"""
+•=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=•
+                                                       GNU GENERAL PUBLIC LICENSE 
+                                                         Version 3, 29 June 2007
+                                                Copyright (C) 2007 Free Software Foundation
+                                            Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
+                                                of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+                                                has been licensed under GNU General Public License
+                                                𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+•=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=••=•
+"""
+import random
+import html
+from datetime import datetime
+import humanize
+from sql.clear_cmd_sql import get_clearcmd
+from HYPE_AFK_BOT import dispatcher
+from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContext, Filters, MessageHandler, run_async
+from sql import afk_sql as sql
+from MISCL import delete
+from telegram import MessageEntity, Update, ParseMode
+from telegram.error import BadRequest
+
+AFK_GROUP = 7
+AFK_REPLY_GROUP = 8
+
+
+def afk(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split(None, 1)
+    user = update.effective_user
+    chat = update.effective_chat
+
+    if not user:  # ignore channels
+        return
+
+    if user.id in [777000, 1087968824]:
+        return
+
+    notice = ""
+    if len(args) >= 2:
+        reason = args[1]
+        if len(reason) > 100:
+            reason = reason[:100]
+            notice = "\nYour afk reason was shortened to 100 characters."
+    else:
+        reason = ""
+
+    sql.set_afk(update.effective_user.id, reason)
+    fname = update.effective_user.first_name
+    try:
+        delmsg = update.effective_message.reply_text(
+            "{} is now away!{}".format(fname, notice))
+
+        cleartime = get_clearcmd(chat.id, "afk")
+
+        if cleartime:
+            context.dispatcher.run_async(delete, delmsg, cleartime.time)
+
+    except BadRequest:
+        pass
+
+
+def no_longer_afk(update: Update, context: CallbackContext):
+    user = update.effective_user
+    message = update.effective_message
+    chat = update.effective_chat
+
+    if not user:  # ignore channels
+        return
+
+    res = sql.rm_afk(user.id)
+    if res:
+        if message.new_chat_members:  # dont say msg
+            return
+        firstname = update.effective_user.first_name
+        try:
+            options = [
+                "{} is here!",
+                "{} is back!",
+                "{} is now in the chat!",
+                "{} is awake!",
+                "{} is back online!",
+                "{} is finally here!",
+                "Welcome back! {}",
+                "Where is {}?\nIn the chat!",
+            ]
+            chosen_option = random.choice(options)
+            delmsg = update.effective_message.reply_text(
+                chosen_option.format(firstname))
+
+            cleartime = get_clearcmd(chat.id, "afk")
+
+            if cleartime:
+                context.dispatcher.run_async(delete, delmsg, cleartime.time)
+
+        except:
+            return
+
+
+def reply_afk(update: Update, context: CallbackContext):
+    bot = context.bot
+    message = update.effective_message
+    userc = update.effective_user
+    userc_id = userc.id
+    if message.entities and message.parse_entities(
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+    ):
+        entities = message.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+        )
+
+        chk_users = []
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                user_id = ent.user.id
+                fst_name = ent.user.first_name
+                chk_users.append(user_id)
+                if user_id in chk_users:
+                    return
+            if ent.type != MessageEntity.MENTION:
+                return
+            chk_users.append(user_id)
+            if user_id in chk_users:
+                return
+            try:
+                chat = bot.get_chat(user_id)
+            except BadRequest:
+                print("Error: Could not fetch userid {} for AFK module".format(user_id))
+                return
+            fst_name = chat.first_name
+            check_afk(update, context, user_id, fst_name, userc_id)
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+        check_afk(update, context, user_id, fst_name, userc_id)
+
+
+def check_afk(update: Update, context: CallbackContext, user_id: int, fst_name: str, userc_id: int):
+    chat = update.effective_chat
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+
+        if int(userc_id) == int(user_id):
+            return
+
+        time = humanize.naturaldelta(datetime.now() - user.time)
+
+        if not user.reason:
+            res = f"{fst_name} is *afk*.\nLast seen: `{time} ago`"
+        else:
+            res = f"{fst_name} is *afk*.\nReason: `{user.reason}`\nLast seen: `{time} ago`"
+
+        delmsg = update.effective_message.reply_text(
+            res,
+            parse_mode=ParseMode.MARKDOWN,
+        )
+
+        cleartime = get_clearcmd(chat.id, "afk")
+
+        if cleartime:
+            context.dispatcher.run_async(delete, delmsg, cleartime.time)
+
+
+def __gdpr__(user_id):
+    sql.rm_afk(user_id)
+
+
+AFK_HANDLER = CommandHandler("afk", afk, run_async=True)
+AFK_REGEX_HANDLER = MessageHandler(
+    Filters.regex(r"^(?i)brb(.*)$"), afk)
+NO_AFK_HANDLER = MessageHandler(
+    Filters.all & Filters.chat_type.groups, no_longer_afk, run_async=True)
+AFK_REPLY_HANDLER = MessageHandler(
+    Filters.all & Filters.chat_type.groups, reply_afk, run_async=True)
+
+dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
+dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
+
+__mod_name__ = "AFK"
+__command_list__ = ["afk"]
+__handlers__ = [
+    (AFK_HANDLER, AFK_GROUP),
+    (AFK_REGEX_HANDLER, AFK_GROUP),
+    (NO_AFK_HANDLER, AFK_GROUP),
+    (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
+]
